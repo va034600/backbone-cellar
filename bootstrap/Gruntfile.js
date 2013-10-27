ff32ac92d456105c43df3042364aa9cf5d91c7a5
eisakuu <eisakuu@mac_sourcetree>
2013-10-27 09:30:00 +0900

    gruntで自動ビルド対応

---
+++ bootstrap/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+	watch: {
+	    files: './js/**',
+	    tasks: ['requirejs']
+	},
+	requirejs: {
+	    compile_main: {
+		options: {
+		    name: 'main',
+		    baseUrl: "./js",
+		    mainConfigFile: './js/main.js',
+		    out: "./build/main.min.js"
+		}
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['requirejs', 'watch']);
+}
